afb8d846801019c51128bd8ba60a54bbc901b3a8
sumitramkr <sumitramkr77e@gmail.com>
2022-12-25 11:23:45 +0530

    delete  menu item completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Invoice from "./components/Invoice";
 import ShowMenu from "./components/ShowMenu";
 import UpdateTax from "./components/UpdateTax";
 import Stats from "./components/Stats";
+import UpdateMenuItem from "./components/UpdateMenuItem";
 
 const App = () => {
   const [authenticated, setAuthenticated] = useState(0);
@@ -56,7 +57,11 @@ const App = () => {
             element={<ShowMenu authenticated={authenticated} />}
           />
           <Route
-            path="stats"
+            path="/updateMenuItem/:food_id"
+            element={<UpdateMenuItem authenticated={authenticated} />}
+          />
+          <Route
+            path="/stats"
             element={<Stats authenticated={authenticated} />}
           />
         </Routes>
