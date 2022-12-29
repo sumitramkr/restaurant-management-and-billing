e96700f77d20d845bf7a3cec0a6a95c411185aa5
sumitramkr <sumitramkr77e@gmail.com>
2022-12-30 01:02:13 +0530

    updated bugs

--- client/src/components/PrintInvoice.jsx
+++ client/src/components/PrintInvoice.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
