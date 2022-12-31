27d3177e101bcaca455ccfc82bc6e1ad40509524
sumitramkr <sumitramkr77e@gmail.com>
2023-01-01 00:18:07 +0530

    refactoring

--- client/src/components/UpdateTax.jsx
+++ client/src/components/UpdateTax.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import axios from "axios";
